1a458ae590c62db7a1e058e00681c51a31ccbc48
Levko Nikitin <53998902+LevkoNikitin@users.noreply.github.com>
2021-10-28 13:32:20 -0400

    Removed unnecessary files

--- testmain.py
+++ testmain.py
@@ -5,28 +5,24 @@ from PIL import ImageTk, Image
 # env variables
 window_size = "1024x600"
 header_sizex, header_sizey = 1024, 114
- 
+
+# global image variable definitions
+
 # Create an instance of tkinter frame
 win = Tk()
 win.title("Substation Technical Training Simulator")
-
-# Set the geometry of frame
 win.geometry("1024x600")
-
-# Create a frame
-header = Frame(win, width=1024, height=114).place(x=0, y=0)
-footer = Frame(win, width=1024, height=64).place(x=0, y=536)
-body = Frame(win, width=1024, height=422).place(x=0, y=100)
 
 
 def draw_header():
     global logo_img
-    logo_img = ImageTk.PhotoImage(Image.open('logo.png'))
-    global UI_image
-    UI_image = ImageTk.PhotoImage(Image.open('./UI/Rectangle 7.png'))
+    logo_img = ImageTk.PhotoImage(Image.open('logo.png'))  # Lakeland Electric Logo
 
-    Label(header, image=logo_img).place(x=0, y=14)
-    Label(header, image=UI_image, text="Substation Technical \n Training Simulator", font=('graphik', 30, 'bold'),
+    global UI_image
+    UI_image = ImageTk.PhotoImage(Image.open('./UI/Rectangle 7.png'))  # Rounded corner label template
+
+    Label(win, image=logo_img).place(x=0, y=14)
+    Label(win, image=UI_image, text="Substation Technical \n Training Simulator", font=('graphik', 30, 'bold'),
           foreground="white", compound="center").place(x=512, y=14)
 
 
@@ -39,15 +35,16 @@ def clear_body_frame():
 def main_page():
     clear_body_frame()
     draw_header()
-    main_name = Label(body, text='main page', font=('nunito', 28), background='grey', foreground='black').pack(pady=20, side="bottom")
-    to_settings_page = Button(body, text='settings', command=settings_page).place(x=600, y=200)
+    main_button_img = ImageTk.PhotoImage(Image.open('./UI/Rectangle 7.png'));
+    Button(win, text='main page', font=('nunito', 30, 'bold'), image=main_button_img,compound='center', foreground='white', width=375, height=75).place(x=324,y=248)
+
 
 
 def settings_page():
     clear_body_frame()
     draw_header()
-    settings_name = Label(body, text='settings page', font=('nunito', 28), background='grey', foreground='black').pack()
-    to_main_page = Button(body, text='main menu', command=main_page).pack(pady=20)
+    settings_name = Label(win, text='settings page', font=('nunito', 28), background='grey', foreground='black').pack()
+    to_main_page = Button(win, text='main menu', command=main_page).pack(pady=20)
 
 
 main_page()
